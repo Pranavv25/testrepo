e35af512866917f56b03d642524b5ace21132855
Pranavv25 <78255930+Pranavv25@users.noreply.github.com>
2022-08-10 16:45:11 +0530

    Add files via upload

---
+++ IBM_Example.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #  My Jupyter Notebook on IBM Watson studio
+
+# __Pranav Vaidya - BE__
+
+# _I am intrested in data science because I want to do something for society and Poor people_
+
+# ### The following code of Adding of two number
+
+# In[2]:
+
+
+num1 = 15
+num2 = 20
+
+abc = num1+num2
+print(abc)
+
+
+# - Hello
+
+# - This is something 
+
+# - That make us happy.
+
+# 1.Feeding Poor to people
+
+# In[ ]:
+
+
+
+
